c8347de3aa9e7c54bed4ce0f112f358f4b667c05
FirstFolder <xinxin.tang92@gmail.com>
2018-03-09 16:57:25 -0500

    Update news_pipeline_launcher.sh

--- news_pipeline_launcher.sh
+++ news_pipeline_launcher.sh
@@ -1,12 +1,3 @@
-# @Author: Dukecat
-# @Date:   2017-05-02T23:27:09-04:00
-# @Last modified by:   Dukecat
-# @Last modified time: 2017-05-11T18:33:28-04:00
-
-
-
-
-
 #!/bin/bash
 
 cd news_pipeline
